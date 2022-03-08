5d769a9fb8b3eb3a35d5aa508aa443e51665d97d
Server Root <virtfac@gmail.com>
2022-03-08 13:43:45 +0000

    Set client .env file and simple static server

---
+++ server/server.js
@@ -0,0 +1,22 @@
+'use strict';
+
+require('dotenv').config()
+const http = require( 'http' );
+const pathUtils = require( 'path' );
+const express = require( 'express' );
+
+const HOSTNAME = process.env.CLIENT_HOSTNAME;
+const PORT = process.env.CLIENT_PORT;
+
+let app = express()
+let appDir = pathUtils.resolve( __dirname, "../dist/" );
+
+app.use(express.static(appDir));
+
+app.get('*', function(_req, res) {
+    res.sendfile( pathUtils.resolve(appDir, 'index.html'));
+});
+
+http.createServer( app ).listen(PORT, HOSTNAME, function() {
+    console.log( `Serve start on http://${HOSTNAME}:${PORT}`);
+});
