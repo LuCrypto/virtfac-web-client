6e8083a934eafb1748627c8ef387b60c16b53310
Misslin Quentin <misslin.quentin@gmail.com>
2022-04-28 19:22:03 +0200

    Update Account and Login view for Unreal

--- src/components/Account.vue
+++ src/components/Account.vue
@@ -1,69 +1,74 @@
 <template>
-  <v-card>
-    <v-img height="300px" src="../assets/ergonom_io.png"></v-img>
-    <v-col class="px-5" style="transform: translateY(-50px);">
-      <v-avatar size="150">
-        <v-img :src="user.picture"></v-img>
-      </v-avatar>
-      <v-list-item color="rgba(0, 0, 0, .4)">
-        <v-list-item-content>
-          <v-list-item-title class="title"
-            >{{ user.firstname }} {{ user.lastname }}</v-list-item-title
-          >
-          <v-list-item-subtitle>Alias {{ user.pseudo }}</v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-    </v-col>
-    <v-card-text>
-      <v-simple-table>
-        <template v-slot:default>
-          <tbody>
-            <tr>
-              <td class="text-right primary--text">Firstname</td>
-              <td class="text-left">{{ user.firstname }}</td>
-            </tr>
-            <tr>
-              <td class="text-right primary--text">Lastname</td>
-              <td class="text-left">{{ user.lastname }}</td>
-            </tr>
-            <tr>
-              <td class="text-right primary--text">Login</td>
-              <td class="text-left">{{ user.login }}</td>
-            </tr>
-            <tr>
-              <td class="text-right primary--text">Mail</td>
-              <td class="text-left">{{ user.mail }}</td>
-            </tr>
-            <tr>
-              <td class="text-right primary--text">Phone</td>
-              <td class="text-left">{{ user.phone }}</td>
-            </tr>
-            <tr>
-              <td class="text-right primary--text">Pseudo</td>
-              <td class="text-left">{{ user.pseudo }}</td>
-            </tr>
-          </tbody>
-        </template>
-      </v-simple-table>
-    </v-card-text>
-    <v-card-actions class="pb-5">
-      <v-spacer></v-spacer>
-      <v-btn color="primary" @click="logout" dark>
-        Log out
-      </v-btn>
-    </v-card-actions>
+  <v-card :rounded="unreal.check() ? 'xl' : 'md'">
+    <v-container>
+      <v-row align="center" justify="center" no-gutters>
+        <v-avatar size="150">
+          <v-img :src="user.picture"></v-img>
+        </v-avatar>
+      </v-row>
+      <v-row align="center" justify="center">
+        <h1 class="title">{{ user.firstname }} {{ user.lastname }}</h1>
+      </v-row>
+      <v-row align="center" justify="center" class="pt-2" no-gutters>
+        <h2 class="subtitle-1">Alias {{ user.pseudo }}</h2>
+      </v-row>
+      <v-row align="center" justify="center" class="pt-2" no-gutters>
+        <v-btn color="primary black--text" @click="logout" dark>
+          Log out
+        </v-btn>
+      </v-row>
+    </v-container>
+
+    <v-simple-table>
+      <template v-slot:default>
+        <tbody>
+          <tr>
+            <td style="width: 50%" class="text-right primary--text">
+              Firstname
+            </td>
+            <td style="width: 50%" class="text-left">{{ user.firstname }}</td>
+          </tr>
+          <tr>
+            <td style="width: 50%" class="text-right primary--text">
+              Lastname
+            </td>
+            <td style="width: 50%" class="text-left">{{ user.lastname }}</td>
+          </tr>
+          <tr>
+            <td style="width: 50%" class="text-right primary--text">Login</td>
+            <td style="width: 50%" class="text-left">{{ user.login }}</td>
+          </tr>
+          <tr>
+            <td style="width: 50%" class="text-right primary--text">Mail</td>
+            <td style="width: 50%" class="text-left">{{ user.mail }}</td>
+          </tr>
+          <tr>
+            <td style="width: 50%" class="text-right primary--text">Phone</td>
+            <td style="width: 50%" class="text-left">{{ user.phone }}</td>
+          </tr>
+          <tr>
+            <td style="width: 50%" class="text-right primary--text">
+              Pseudo
+            </td>
+            <td style="width: 50%" class="text-left">{{ user.pseudo }}</td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
   </v-card>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import { Session, User } from '@/utils/session'
+import Unreal from '@/utils/unreal'
 
 @Component({
   components: {}
 })
-export default class AccountPopUp extends Vue {
+export default class Account extends Vue {
   user: User | null = null
+  unreal = Unreal
 
   created (): void {
     this.user = Session.getUser()
