9f06425a29519efe7f73d3e4dd140fbcf7c67753
Misslin Quentin <misslin.quentin@gmail.com>
2022-06-09 12:15:46 +0200

    Add dark/light style switch on CodeStructure documentation

--- src/views/APIDocumentation/CodeStructure.vue
+++ src/views/APIDocumentation/CodeStructure.vue
@@ -1,10 +1,45 @@
 <template>
-  <iframe src="./doc/index.html" frameborder="0" />
+  <iframe
+    style="display: block;"
+    ref="iframe"
+    src="./doc/index.html"
+    @load="frameLoaded"
+    frameborder="0"
+  />
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 
 @Component
-export default class CodeStructure extends Vue {}
+export default class CodeStructure extends Vue {
+  iframe: HTMLIFrameElement | null = null
+  iframeDefaultHead = ''
+
+  mounted (): void {
+    this.iframe = this.$refs.iframe as HTMLIFrameElement
+    this.$root.$on('changeDarkMode', () => {
+      this.updateTheme()
+    })
+  }
+
+  frameLoaded (): void {
+    if (!this.iframe || !this.iframe.contentDocument) {
+      return
+    }
+    this.iframeDefaultHead = this.iframe.contentDocument.head.innerHTML
+    this.updateTheme()
+  }
+
+  updateTheme (): void {
+    if (!this.iframe || !this.iframe.contentDocument) {
+      return
+    }
+    this.iframe.contentDocument.head.innerHTML = `${
+      this.iframeDefaultHead
+    }<link rel="stylesheet" href="../docStyle${
+      this.$vuetify.theme.dark ? 'dark' : 'light'
+    }.css"></link>`
+  }
+}
 </script>
