ed88a249610f888bdee68fa3a708d6dc5b334927
Misslin Quentin <misslin.quentin@gmail.com>
2022-06-14 18:30:33 +0200

    Update dynamics in put with open file

--- src/views/Dynamics/DynamicsInput.vue
+++ src/views/Dynamics/DynamicsInput.vue
@@ -1,16 +1,34 @@
 <template>
-  <v-container fluid class="d-flex">
-    <v-card class="pa-6 ma-6">
-      Dynamics Input
-    </v-card>
+  <v-container fluid class="d-flex flex-column">
+    <v-col class="flex-grow-0">
+      <v-card flat class="flex-grow-1">
+        <v-card-title>Dynamics inputs</v-card-title>
+        <v-card-subtitle
+          >This tool allows you to upload dynamics simulation files from
+          Witness, PlantSimulation or Sympy. These files are then used for
+          example by the routing analysis tool.</v-card-subtitle
+        >
+      </v-card>
+    </v-col>
+    <v-col class="flex-grow-1" style="overflow-y: auto;">
+      <open-file
+        style="height: 100%;"
+        application="ERGONOM_IO"
+        :header="false"
+      ></open-file
+    ></v-col>
   </v-container>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
+import OpenFile from '@/components/OpenFile.vue'
 
 @Component({
-  name: 'DynamicsInput'
+  name: 'DynamicsInput',
+  components: {
+    OpenFile
+  }
 })
 // @vuese
 // @group VIEWS
