934578b6647be08a9f8e2091497304fa776816f6
Misslin Quentin <misslin.quentin@gmail.com>
2022-07-21 17:41:18 +0200

    Update Odoo iframes to use odoo-i-frame component

--- src/views/ContradictionAnalysis/AboutContradictionAnalysis.vue
+++ src/views/ContradictionAnalysis/AboutContradictionAnalysis.vue
@@ -1,19 +1,20 @@
 <template>
-  <v-container fluid class="d-flex pa-0">
-    <iframe v-if="page" class="flex-grow-1 fill-height" :src="page"></iframe>
-    <div v-else>No page to display</div>
-  </v-container>
+  <odoo-i-frame
+    src="http://10.244.77.203/about-contradiction-analysis"
+  ></odoo-i-frame>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
+import OdooIFrame from '@/components/OdooIframe.vue'
 
 @Component({
-  name: 'AboutContradictionAnalysis'
+  name: 'AboutContradictionAnalysis',
+  components: {
+    OdooIFrame
+  }
 })
 // @vuese
 // @group VIEWS
-export default class AboutContradictionAnalysis extends Vue {
-  page = 'http://10.244.77.203/about-contradiction-analysis'
-}
+export default class AboutContradictionAnalysis extends Vue {}
 </script>
